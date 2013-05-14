2be2c9a018276044f269df2aa84ead1d23676138
Vikram Dighe <vikram@swiftsignal.com>
2013-05-14 00:19:59 -0400

    Created MadLib and Solution models. Passing tests.

---
+++ app/models/mad_lib.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: mad_libs
+#
+#  id   :integer          not null, primary key
+#  text :text
+#
+
+class MadLib < ActiveRecord::Base
+  attr_accessible :text
+  attr_accessor :solutions, :hashes
+
+  after_initialize do |mad_lib|
+    self.hashes = Hash.new
+    mad_lib.text.scan(/{(.*?)}/).flatten.inject(Hash.new(0)) {|h, (k, v)| h[k.capitalize] = h[k.capitalize] + 1 ; h}.each {|k,v| v.times {|i| self.hashes[k + ' (' + (i + 1).to_s + '):'] = nil}} # This may be a bit much.
+    self.solutions = Solution.new(mad_lib)
+  end
+
+  def has_field?(field)
+    self.hashes.has_key? field
+  end
+end
---
+++ app/models/solution.rb
@@ -0,0 +1,22 @@
+class Solution
+  attr_accessor :mad_lib
+
+  def initialize(mad_lib)
+    self.mad_lib = mad_lib
+  end
+
+  def create
+    self
+  end
+
+  def fill_field(label, values)
+    @hashes = self.mad_lib.hashes
+    @hashes[label] = values[:with]
+  end
+
+  def resolve
+    text = self.mad_lib.text
+    self.mad_lib.hashes.each {|k,v| text.sub!('{' + k.split(/ \(|\)/)[0].downcase + '}', v.to_s)}
+    text
+  end
+end
